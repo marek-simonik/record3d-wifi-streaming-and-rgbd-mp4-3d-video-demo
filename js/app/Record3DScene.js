27869461bcada30346ac6a02823d19471cf1fe47
Remy Mellet <remy.mellet@gmail.com>
2023-01-05 15:14:07 +0100

    because of unpkg.com lasting >30s to provide https://unpkg.com/three@0.147.0/examples/jsm/libs/lil-gui.module.min.js switch to another cdn - note that jsdeliver doesnt fallback to filename.js when only filename in import

--- js/app/Record3DScene.js
+++ js/app/Record3DScene.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js'
 import {Record3DVideoRenderingMode} from "./Record3DVideo.js"
 
